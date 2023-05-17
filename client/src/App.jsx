e2ff45b0d609be067ce52826299c3a108c7e0a6c
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-05-16 17:50:29 -0700

    added generic padding for development

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/Header/Header";
 import Ratings from "./components/Ratings/Ratings";
 import Camping_spots from "./components/Camping_spots/Camping_spots";
@@ -10,21 +11,21 @@ function App() {
             <div>
                 <Header />
             </div>
-            <h1>Campsite name, rating, location (Genglin)</h1>
+            <h1 className="padding-top-xl">Campsite name, rating, location (Genglin)</h1>
             <h1>Photo Gallery (Dennis)</h1>
             <h1>Campsite info, acres, etc. (Chris)</h1>
             <h1>Available Sites (Genglin)</h1>
             <h1>Tent Sites (plus map)(Chris)</h1>
             <h1>Location (Dan)</h1>
-            <h1>Hosted by (Dennis)</h1>
+            <h1 className="padding-bottom">Hosted by (Dennis)</h1>
             <div>
                 <Ratings />
             </div>
-            <h1>Things to do (Dan)</h1>
+            <h1 className="padding-top padding-bottom">Things to do (Dan)</h1>
             <div>
                 <Camping_spots />
             </div>
-            <h1>Campers also viewed (Chris)</h1>
+            <h1 className="padding-top">Campers also viewed (Chris)</h1>
             <h1>Camp Safety (Dennis)</h1>
             <h1>Safety & Partners (Dan)</h1>
             <h1>Footer (Chris)</h1>
